550bb28cbf4d2a3841d2160eeab8fce5b8396860
Kazuya O'moto <komoto@jeol.co.jp>
2023-02-25 21:28:39 +0900

    Modify deb; Use mwx.app

--- __init__.py
+++ __init__.py
@@ -7,13 +7,9 @@ from .wxNautilus import MainFrame as Frame
 
 
 def deb(target=None, loop=True, **kwargs):
-    import wx
     import mwx
     from .debut import main
-    app = wx.GetApp() or wx.App()
-    frame = mwx.deb(target, loop=0, **kwargs) # Don't enter loop.
-    frame.Show()
-    main(frame)
-    if loop and not app.GetMainLoop():
-        app.MainLoop()
-    return frame
+    with mwx.app(loop):
+        frame = mwx.deb(target, loop=0, **kwargs) # Don't enter loop.
+        main(frame)
+        return frame
