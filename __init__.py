5e8f6eb911cfa85b8484b2a307b2b6b5ec20c5e5
Kazuya O'moto <komoto@jeol.co.jp>
2023-03-22 16:44:07 +0900

    Add app context mgr

--- __init__.py
+++ __init__.py
@@ -2,14 +2,25 @@
 # -*- coding: utf-8 -*-
 """Nautilus package
 """
+import contextlib
+
 from mwx.graphman import Layer
 from .wxNautilus import MainFrame as Frame
+
+
+@contextlib.contextmanager
+def app(loop=True):
+    import wx
+    app = wx.GetApp() or wx.App()
+    yield app
+    if loop and not app.GetMainLoop():
+        app.MainLoop()
 
 
 def deb(target=None, loop=True, **kwargs):
     import mwx
     from .debut import main
-    with mwx.app(loop):
+    with app(loop):
         frame = mwx.deb(target, loop=0, **kwargs) # Don't enter loop.
         main(frame)
         return frame
