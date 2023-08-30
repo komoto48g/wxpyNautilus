7e89fb1e792e1fa12022236678308446a72efd8e
Kazuya O'moto <komoto@jeol.co.jp>
2023-08-30 17:26:02 +0900

    little tweaks

--- wxNautilus.py
+++ wxNautilus.py
@@ -47,8 +47,9 @@ class MainFrame(Frame):
         
         home = os.path.dirname(__file__)
         
-        if home not in sys.path: # Add home (when importing Frame class)
+        if home not in sys.path: # Add home (to import Frame)
             sys.path.insert(0, home)
+        
         sys.path.insert(0, os.path.join(home, 'plugins'))
         sys.path.insert(0, '') # Add local . to import si:local first
         try:
