f7ebcfb8c172c890e9b44e08cc17d87ea63189d9
Kazuya O'moto <komoto@jeol.co.jp>
2023-09-05 00:35:42 +0900

    Remove unused propperties

--- plugins/viewframe.py
+++ plugins/viewframe.py
@@ -252,16 +252,6 @@ class Plugin(Layer):
     def all_pages(self):
         return [self.nb.GetPage(i) for i in range(self.nb.PageCount)]
     
-    @property
-    def selected_buffers(self):
-        page = self.nb.CurrentPage
-        return page.Target[page.selected_items]
-    
-    @property
-    def focused_buffer(self):
-        page = self.nb.CurrentPage
-        return page.Target[page.focused_item]
-    
     def Init(self):
         self.nb = aui.AuiNotebook(self, size=(400,150),
             style = (aui.AUI_NB_DEFAULT_STYLE|aui.AUI_NB_RIGHT)
