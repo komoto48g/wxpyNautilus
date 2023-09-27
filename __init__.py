bf97256763fa96b3d219ff2bb871128324ecbb7a
Kazuya O'moto <komoto@jeol.co.jp>
2023-09-28 00:16:52 +0900

    Rename app --> App; Remove App --> upstream

--- __init__.py
+++ __init__.py
@@ -2,25 +2,15 @@
 # -*- coding: utf-8 -*-
 """Nautilus package
 """
-import contextlib
-
+from mwx.framework import App
 from mwx.graphman import Layer
 from .wxNautilus import MainFrame as Frame
-
-
-@contextlib.contextmanager
-def app(loop=True):
-    import wx
-    app = wx.GetApp() or wx.App()
-    yield app
-    if loop and not app.GetMainLoop():
-        app.MainLoop()
 
 
 def deb(target=None, loop=True, **kwargs):
     import mwx
     from .debut import main
-    with app(loop):
+    with App(loop):
         frame = mwx.deb(target, loop=0, **kwargs) # Don't enter loop.
         main(frame)
         return frame
